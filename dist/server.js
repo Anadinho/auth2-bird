ff5daef521dbfa8c42e6db379a6d3eab6565616d
Gabriel Anadinho <gabrielanadinho@gmail.com>
2021-09-20 16:58:00 -0300

    asdfgh

--- dist/server.js
+++ dist/server.js
@@ -8,6 +8,6 @@ var routes_1 = __importDefault(require("./routes"));
 var app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use(routes_1.default);
-app.listen(3333, function () {
+app.listen(process.env.PORT || 3333, function () {
     console.log('server started on port 3333');
 });
